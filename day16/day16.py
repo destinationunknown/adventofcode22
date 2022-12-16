80a5620d65bdfd5d0266c3f0406fc5c9d1dc271c
Varun Ramakrishnan <ravarun3@gmail.com>
2022-12-16 13:32:05 +0530

    Solve Day 16

--- day16/day16.py
+++ day16/day16.py
@@ -1,8 +1,6 @@
 from functools import lru_cache
 import sys
 
-from networkx.convert_matrix import itertools
-
 sys.setrecursionlimit(100000000)
 data = open("input.txt", "r").read().strip().split("\n")
 
@@ -41,33 +39,26 @@ def part_two(data: list[str]):
     graph = parse_input(data)
 
     @lru_cache(maxsize=None)
-    def helper(person, elephant, time_left, opened: frozenset):
+    def helper(node, time_left, opened: frozenset, player_turn):
         if time_left <= 0:
-            return 0
+            if player_turn:
+                return helper("AA", 26, opened, False)
+            else:
+                return 0
         else:
             results = [
-                helper(p, e, time_left - 1, opened)
-                for p, e in itertools.product(graph[person][1], graph[elephant][1])
+                helper(n, time_left - 1, opened, player_turn) for n in graph[node][1]
             ]
-            if person not in opened and graph[person][0] > 0:
+            if node not in opened and graph[node][0] > 0:
+                val = (time_left - 1) * graph[node][0]
                 _opened = set(opened)
-                val = (time_left - 1) * graph[person][0]
-                _opened.add(person)
+                _opened.add(node)
                 results += [
-                    helper(person, e, time_left - 1, frozenset(_opened)) + val
-                    for e in graph[elephant][1]
-                ]
-            if elephant not in opened and graph[elephant][0] > 0:
-                _opened = set(opened)
-                val = (time_left - 1) * graph[elephant][0]
-                _opened.add(elephant)
-                results += [
-                    helper(p, elephant, time_left - 1, frozenset(_opened)) + val
-                    for p in graph[person][1]
+                    helper(node, time_left - 1, frozenset(_opened), player_turn) + val
                 ]
             return max(results)
 
-    return helper("AA", "AA", 26, frozenset())
+    return helper("AA", 26, frozenset(), True)
 
 
 if __name__ == "__main__":
