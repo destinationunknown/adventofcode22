669b1408fad0dbab98d20b2f4b49501918729390
Varun Ramakrishnan <ravarun3@gmail.com>
2022-12-16 13:25:04 +0530

    Parallel approach

--- day16/day16.py
+++ day16/day16.py
@@ -1,8 +1,10 @@
 from functools import lru_cache
 import sys
 
+from networkx.convert_matrix import itertools
+
 sys.setrecursionlimit(100000000)
-data = open("test.txt", "r").read().strip().split("\n")
+data = open("input.txt", "r").read().strip().split("\n")
 
 
 def parse_input(data: list[str]):
@@ -29,14 +31,43 @@ def part_one(data: list[str]):
                 val = (time_left - 1) * graph[node][0]
                 _opened = set(opened)
                 _opened.add(node)
-                results += [helper(node, time_left - 2, frozenset(_opened)) + val]
+                results += [helper(node, time_left - 1, frozenset(_opened)) + val]
             return max(results)
 
     return helper("AA", 30, frozenset())
 
 
 def part_two(data: list[str]):
-    ...
+    graph = parse_input(data)
+
+    @lru_cache(maxsize=None)
+    def helper(person, elephant, time_left, opened: frozenset):
+        if time_left <= 0:
+            return 0
+        else:
+            results = [
+                helper(p, e, time_left - 1, opened)
+                for p, e in itertools.product(graph[person][1], graph[elephant][1])
+            ]
+            if person not in opened and graph[person][0] > 0:
+                _opened = set(opened)
+                val = (time_left - 1) * graph[person][0]
+                _opened.add(person)
+                results += [
+                    helper(person, e, time_left - 1, frozenset(_opened)) + val
+                    for e in graph[elephant][1]
+                ]
+            if elephant not in opened and graph[elephant][0] > 0:
+                _opened = set(opened)
+                val = (time_left - 1) * graph[elephant][0]
+                _opened.add(elephant)
+                results += [
+                    helper(p, elephant, time_left - 1, frozenset(_opened)) + val
+                    for p in graph[person][1]
+                ]
+            return max(results)
+
+    return helper("AA", "AA", 26, frozenset())
 
 
 if __name__ == "__main__":
