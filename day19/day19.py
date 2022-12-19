8fb0871abef05b2439555d6573ab5a637aadd956
Varun Ramakrishnan <ravarun3@gmail.com>
2022-12-19 12:19:11 +0530

    Solve Day 19

--- day19/day19.py
+++ day19/day19.py
@@ -1,9 +1,10 @@
 import functools
+import math
 import sys
 
 sys.setrecursionlimit(100000000)
 
-data = open("test.txt", "r").read().strip().split("\n")
+data = open("input.txt", "r").read().strip().split("\n")
 
 
 class Blueprint:
@@ -13,12 +14,18 @@ class Blueprint:
         self.obsidian_cost = obsidian_cost
         self.geode_cost = geode_cost
 
+        # try to limit the state space by defining a max number of robots we could possibly need
+        # if the amount of resources we gain in 1 min is more than the max amount of that resource we could spend in one minute,
+        # we don't need any more robots
+        self.max_ore_r = max(ore_cost, clay_cost, obsidian_cost[0], geode_cost[0])
+        self.max_clay_r = obsidian_cost[1]
+        self.max_obsidian_r = geode_cost[1]
+
 
 def parse_input(data: list[str]):
     blueprints = []
     for line in data:
         blueprint = line[line.index(":") + 2 :].split(".")
-        print(blueprint)
 
         ore_cost = int(blueprint[0].strip().split(" ")[4])
         clay_cost = int(blueprint[1].strip().split(" ")[4])
@@ -46,91 +53,97 @@ def assess_blueprint(
     obsidian_r,
     geode_r,
     minute,
+    max_time,
     blueprint: Blueprint,
 ) -> int:
-    if minute > 24:
+    if minute > max_time:
         return geode
 
-    ore += ore_r
-    clay += clay_r
-    obsidian += obsidian_r
-    geode += geode_r
+    # try to limit the state space by defining maximum amount of each possible resource we could use
+    ore = min((max_time + 1 - minute) * blueprint.max_ore_r, ore)
+    clay = min((max_time + 1 - minute) * blueprint.max_clay_r, clay)
+    obsidian = min((max_time + 1 - minute) * blueprint.max_obsidian_r, obsidian)
 
     possibilities = []
 
     if ore >= blueprint.geode_cost[0] and obsidian >= blueprint.geode_cost[1]:
         possibilities.append(
             assess_blueprint(
-                ore - blueprint.geode_cost[0],
-                clay,
-                obsidian - blueprint.geode_cost[1],
-                geode,
+                ore + ore_r - blueprint.geode_cost[0],
+                clay + clay_r,
+                obsidian + obsidian_r - blueprint.geode_cost[1],
+                geode + geode_r,
                 ore_r,
                 clay_r,
                 obsidian_r,
                 geode_r + 1,
-                minute - 1,
+                minute + 1,
                 blueprint,
             )
         )
 
-    elif ore >= blueprint.obsidian_cost[0] and clay >= blueprint.obsidian_cost[1]:
+    if (
+        obsidian_r < blueprint.max_obsidian_r
+        and ore >= blueprint.obsidian_cost[0]
+        and clay >= blueprint.obsidian_cost[1]
+    ):
         possibilities.append(
             assess_blueprint(
-                ore - blueprint.obsidian_cost[0],
-                clay - blueprint.obsidian_cost[1],
-                obsidian,
-                geode,
+                ore + ore_r - blueprint.obsidian_cost[0],
+                clay + clay_r - blueprint.obsidian_cost[1],
+                obsidian + obsidian_r,
+                geode + geode_r,
                 ore_r,
                 clay_r,
                 obsidian_r + 1,
                 geode_r,
-                minute - 1,
+                minute + 1,
                 blueprint,
             )
         )
 
-    elif clay >= blueprint.clay_cost:
+    if clay_r < blueprint.max_clay_r and ore >= blueprint.clay_cost:
         possibilities.append(
             assess_blueprint(
-                ore,
-                clay - blueprint.clay_cost,
-                obsidian,
-                geode,
+                ore + ore_r - blueprint.clay_cost,
+                clay + clay_r,
+                obsidian + obsidian_r,
+                geode + geode_r,
                 ore_r,
                 clay_r + 1,
                 obsidian_r,
                 geode_r,
-                minute - 1,
+                minute + 1,
                 blueprint,
             )
         )
-    elif ore >= blueprint.ore_cost:
+    if ore_r < blueprint.max_ore_r and ore >= blueprint.ore_cost:
         possibilities.append(
             assess_blueprint(
-                ore - blueprint.ore_cost,
-                clay,
-                obsidian,
-                geode,
+                ore + ore_r - blueprint.ore_cost,
+                clay + clay_r,
+                obsidian + obsidian_r,
+                geode + geode_r,
                 ore_r + 1,
                 clay_r,
                 obsidian_r,
                 geode_r,
-                minute - 1,
+                minute + 1,
                 blueprint,
             )
         )
+
     possibilities.append(
         assess_blueprint(
-            ore,
-            clay,
-            obsidian,
-            geode,
+            ore + ore_r,
+            clay + clay_r,
+            obsidian + obsidian_r,
+            geode + geode_r,
             ore_r,
             clay_r,
             obsidian_r,
             geode_r,
-            minute - 1,
+            minute + 1,
             blueprint,
         )
     )
@@ -140,11 +153,20 @@ def assess_blueprint(
 
 def part_one(data: list[str]):
     blueprints = parse_input(data)
-    return assess_blueprint(0, 0, 0, 0, 1, 0, 0, 0, 1, blueprints[0])
+    q = 0
+    for i, blueprint in enumerate(blueprints):
+        print(i)
+        q += (i + 1) * assess_blueprint(0, 0, 0, 0, 1, 0, 0, 0, 1, 26, blueprint)
+    return q
 
 
 def part_two(data: list[str]):
-    ...
+    blueprints = parse_input(data)
+    q = 1
+    for i, blueprint in enumerate(blueprints[:3]):
+        print(i)
+        q *= assess_blueprint(0, 0, 0, 0, 1, 0, 0, 0, 1, 32, blueprint)
+    return q
 
 
 if __name__ == "__main__":
