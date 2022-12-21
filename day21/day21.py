7803c099bd163d35be9c84d73328d46fb509c609
Varun Ramakrishnan <ravarun3@gmail.com>
2022-12-21 11:32:07 +0530

    Solve by printing equation

--- day21/day21.py
+++ day21/day21.py
@@ -1,18 +1,14 @@
 import operator
+from sympy import solve, sympify
 
-data = open("test.txt", "r").read().strip().split("\n")
+data = open("input.txt", "r").read().strip().split("\n")
 
 ops = {"+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.floordiv}
 
 
 def part_one(data: list[str]) -> int:
     monkeys = [line.split(" ") for line in data]
-
-    # monkeys that are waiting for key
-    waiting = {}
-
-    # set of monkeys that key requires
-    jobs = {}
+    jobs = []
 
     vals = {}
 
@@ -22,40 +18,55 @@ def part_one(data: list[str]) -> int:
             val = int(line[1])
             vals[monkey] = val
 
-            if monkey in waiting:
-                q = []
-                q.append(waiting[monkey])
-
-                while q:
-                    satisfied = q.pop(0)
-                    if all([dep in vals for dep in jobs[satisfied][0]]):
-                        op = jobs[satisfied][1]
-                        a = jobs[satisfied][0][0]
-                        b = jobs[satisfied][0][1]
-
-                        vals[satisfied] = op(vals[a], vals[b])
-                        if satisfied in waiting:
-                            q.append(waiting[satisfied])
-
         else:
             a = line[1]
-            op = ops[line[2]]
+            op = line[2]
             b = line[3]
 
-            if a in vals and b in vals:
-                vals[monkey] = op(vals[a], vals[a])
-            else:
-                jobs[monkey] = [(a, b), op]
-                waiting[a] = monkey
-                waiting[b] = monkey
+            jobs.append((monkey, (a, b), op))
 
-    print([j for j in jobs if j not in vals])
+    while len(jobs) > 0:
+        job = jobs.pop(0)
+        m, (a, b), op = job
+        if a in vals and b in vals:
+            vals[m] = ops[op](vals[a], vals[b])
+        else:
+            jobs.append(job)
+
     return vals["root"]
-    return 0
 
 
 def part_two(data: list[str]) -> int:
-    ...
+    monkeys = [line.split(" ") for line in data]
+    jobs = []
+
+    vals = {}
+
+    for line in monkeys:
+        monkey = line[0][:-1]
+        if len(line) == 2:
+            val = line[1]
+            vals[monkey] = val
+
+        else:
+            a = line[1]
+            op = line[2]
+            b = line[3]
+
+            jobs.append((monkey, (a, b), op))
+
+    while len(jobs) > 0:
+        vals["humn"] = "x"
+        job = jobs.pop(0)
+        m, (a, b), op = job
+        if m == "root":
+            op = "="
+        if a in vals and b in vals:
+            vals[m] = f"({vals[a]} {op} {vals[b]})"
+        else:
+            jobs.append(job)
+
+    print(vals["root"])
 
 
 if __name__ == "__main__":
